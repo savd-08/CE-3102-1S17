866fb7203b2aaa9f2fc8e7b26e07f7620c1a3316
Andres Brais <abcbrais@gmail.com>
2017-02-24 22:00:27 -0600

    se termino con las graficas y se hizo el documento con el resultado de los metodos

--- Tarea2/tarea_2.m
+++ Tarea2/tarea_2.m
@@ -1,11 +1,14 @@
-h_arr = [1, 1e-1 , 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10];
-
+#Arreglo de exponente para la h
+exponente = 0:20;
+#Arreglo para el calculo de las h
+h_arreglo = 10 .^ (-exponente);
+#Valor de donde se evalua la funcion
 x = 1;
-
+#Valor real de la derivada
 valor_real = 0.9;
 
 
-
+#Funcion que calcula el valor de la funcion en cuestion en una posicion x
 function retval = f_x  (x)
   if(isscalar(x))
     retval = (0.3*(x**4)) - (0.15*(x**2));
@@ -13,30 +16,39 @@ function retval = f_x  (x)
     error("f_x: se espera un argumento escalar");
   endif
 endfunction
-
+#Funcion que calcula el valor por diferencia centrada
 function retval = d_centrada (x,h)
   retval = (f_x(x+h) - f_x(x-h))/(2*h);
 endfunction
-
+#funcion que calcula el valor de la diferencia hacia atras
 function retval = d_atras (x,h)
   retval = (f_x(x) - f_x(x-h))/h;
 endfunction
-
+#funcion que calcula el valor de la diferencia hacia delante
 function retval = d_adelante (x,h)
   retval = (f_x(x+h) - f_x(x))/h;
 endfunction
 
+#calulo de los errores
+err_centrada = abs(valor_real .- arrayfun(@d_centrada, x, h_arreglo));
 
-err_centrada = valor_real .- arrayfun(@d_centrada, x, h_arr);
+err_atras = abs(valor_real .- arrayfun(@d_atras, x, h_arreglo));
 
-err_atras = valor_real .- arrayfun(@d_atras, x, h_arr);
+err_adelante = abs(valor_real .- arrayfun(@d_adelante, x, h_arreglo));
 
-err_adelante = valor_real .- arrayfun(@d_adelante, x, h_arr);
+#graficacion de los erroes respecto a la diferencia
+loglog(h_arreglo, err_centrada);
+xlabel("log de la diferencia");
+ylabel("log del error");
+title("Diferencias Centradas")
 
+loglog(h_arreglo, err_atras);
+xlabel("log de la diferencia");
+ylabel("log del error");
+title("Diferencias Hacia Atras")
 
-disp("Error centrada: "), disp(err_centrada);
-
-disp("Error atras: "), disp(err_atras);
-
-disp("Error adelante: "), disp(err_adelante);
+loglog(h_arreglo, err_adelante);
+xlabel("log de la diferencia");
+ylabel("log del error");
+title("Diferencias Delante")
 
