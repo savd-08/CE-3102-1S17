290a97399e2c6300ac22f839809da8548248db91
Andres Brais <abcbrais@gmail.com>
2017-03-26 11:53:08 -0600

    el algorimo de muller permite el uso de polinomios en vez de funciones

--- Proyecto 2/include/muller.h
+++ Proyecto 2/include/muller.h
@@ -1,10 +1,9 @@
 #include <complex>
 #include <limits>
+#include <cmath>
+#include "boost_poly.h"
 
-#ifndef anpi_f
-#define anpi_f
-
-namespace anpi{
+namespace boost{ namespace math{ namespace tools{
 
   template <class T>
   class muller
@@ -14,16 +13,16 @@ namespace anpi{
       unsigned int _iters;
       //tolerancia
       T _precision;
-      //funcion
-      std::complex<T>(*f_x)(std::complex<T> x);
+      //polinomio
+      polynomial<std::complex<T>> _polinomio;
 
     public:
 
       //constructor
-      muller(std::complex<T>(*pf_x)(std::complex<T> x),T precision = std::sqrt(std::numeric_limits<T>::epsilon()), unsigned int iters = 1000){
+      muller(polynomial<std::complex<T>> polinomio,T precision = std::sqrt(std::numeric_limits<T>::epsilon()), unsigned int iters = 1000){
         _precision = precision;
         _iters = iters;
-        f_x = pf_x;
+        _polinomio = polinomio;
       }
 
       std::complex<T> operator()(T x1, T x2){
@@ -32,8 +31,8 @@ namespace anpi{
         std::complex<T> cx0 = x1;
         std::complex<T> cx2 = x2;
 
-        std::complex<T> cy0 = f_x(cx0);
-        std::complex<T> cy2 = f_x(cx2);
+        std::complex<T> cy0 = poly_evaluator(cx0, _polinomio);//f_x(cx0);
+        std::complex<T> cy2 = poly_evaluator(cx2, _polinomio);
 
         //secante para calcular un tercer punto
         std::complex<T> cx1 = (cx2 - cy2)*((cx2 - cx0)/(cy2 - cy0));
@@ -43,14 +42,14 @@ namespace anpi{
           std::complex<T> h1 = cx1 - cx0;
           std::complex<T> h2 = cx2 - cx1;
           //cambio de la funcion
-          std::complex<T> delta1 = (f_x(cx1) - f_x(cx0))/h1;
-          std::complex<T> delta2 = (f_x(cx2) - f_x(cx1))/h2;
+          std::complex<T> delta1 = (poly_evaluator(cx1, _polinomio) - poly_evaluator(cx0, _polinomio))/h1;
+          std::complex<T> delta2 = (poly_evaluator(cx2, _polinomio) - poly_evaluator(cx1, _polinomio))/h2;
           //paso
           std::complex<T> d = (delta2 - delta1)/(h2+h1);
           //calculo de b
           std::complex<T> b = delta2 + (h2*d);
           //calculo de la raíz cuadrada del denomidador
-          std::complex<T> D = std::sqrt((b*b)-(4.0*f_x(cx2)*d));
+          std::complex<T> D = std::sqrt((b*b)-(4.0*poly_evaluator(cx2, _polinomio)*d));
           //temporales para validación de signo
           std::complex<T> tmp1 = b - D;
           std::complex<T> tmp2 = b + d;
@@ -64,11 +63,17 @@ namespace anpi{
             E = b - D;
           }
           //parte que se le resta a x2
-          std::complex<T> h = ((-2.0) * f_x(cx2))/E;
+          std::complex<T> h = ((-2.0) * poly_evaluator(cx2, _polinomio))/E;
           //nueva x
           std::complex<T> p = cx2 + h;
 
           if(std::abs(h) <= _precision){
+            if(std::abs(std::real(p)) <= _precision){
+              p = std::complex<T>(0, std::imag(p));
+            }
+            if(std::abs(std::imag(p)) <= _precision){
+              p = std::complex<T>(std::real(p), 0);
+            }
             return p;
           }
 
@@ -84,6 +89,6 @@ namespace anpi{
 
   };
 
-}
-
-#endif
+} //namespace tools
+} //namespace math
+} //namespace boost
